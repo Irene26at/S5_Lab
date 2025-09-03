dbb6db1febf1a86462728a97deefd477410daa0d
ShanSuharban <shansuharban73@gmail.com>
2025-09-03 20:34:14 +0530

    qn of builtin

---
+++ DBMS_Lab/builin_fn.sql
@@ -0,0 +1,33 @@
+				-- BUILT IN FUNCTIONS --
+
+-- 1. Display the name of employees in upper case --
+
+
+
+-- 2. Display the name of employees in lower case --
+
+-- 3. Display the name of employees in proper case --
+
+-- 4. Display the length of each employee name --
+
+-- 5. Display the name of employees and concatenate it with employee no --
+
+-- 6. Extract 3 characters starting from 2nd character from string ‘Oracle’ --
+
+-- 7. Find the first occurrence of character ‘a’ in the string ‘COMPUTER MAINTENANCE’ --
+
+-- 8. Replace every occurrence of alphabet ‘a’ with ‘b’ in the string ‘MATHEMATICS’ --
+
+-- 9. Display the first 3 characters of all the city in EMPLOYEE table --
+
+-- 10. Display your age in days --
+
+-- 11. Display your age in months --
+
+-- 12. Display the current date and time --
+
+-- 13. Display the date 3 months before the current date --
+
+-- 14. Display the current date in the format ‘DATE-MONTH-DAY’ --
+
+-- 15. Mr/Ms ------ who is ----------- years old is residing in ---------- Display the details of employees in the given format. (Fill blanks with ENAME, AGE, CITY respectively) --

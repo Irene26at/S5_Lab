fc58f2fe8541e7ee2ef24a9284f0bafe711db64b
Muhammed Shan <95335433+ShanSuharban@users.noreply.github.com>
2025-10-28 12:45:37 +0530

    Add process ID tracking to FCFS scheduling

--- SS_Lab/_Lab/fcfs/fcfs-preemptive.c
+++ SS_Lab/_Lab/fcfs/fcfs-preemptive.c
@@ -1,33 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-
+    // Added pid[10] to track original process IDs
     int n, i, j;
-    int at[10], bt[10], ct[10], wt[10], tat[10];
+    int at[10], bt[10], ct[10], wt[10], tat[10], pid[10];
     int temp;
     float totaltat = 0, totalwt = 0;
 
     printf("Enter the total number of processes: ");
     scanf("%d", &n);
 
-
     for (i = 0; i < n; i++) {
-        printf("Enter AT of process [%d]: ", i + 1);
+        pid[i] = i + 1; // Store the original process ID (P1, P2, etc.)
+        printf("Enter AT of process [%d]: ", pid[i]);
         scanf("%d", &at[i]);
-        printf("Enter BT of process [%d]: ", i + 1);
+        printf("Enter BT of process [%d]: ", pid[i]);
         scanf("%d", &bt[i]);
     }
 
-
+    // Sorting based on Arrival Time (at)
     for (i = 0; i < n - 1; i++) {
         for (j = i + 1; j < n; j++) {
             if (at[i] > at[j]) {
+                // Swap Process ID
+                temp = pid[i];
+                pid[i] = pid[j];
+                pid[j] = temp;
 
+                // Swap Arrival Time
                 temp = at[i];
                 at[i] = at[j];
                 at[j] = temp;
 
-
+                // Swap Burst Time
                 temp = bt[i];
                 bt[i] = bt[j];
                 bt[j] = temp;
@@ -35,12 +40,14 @@ int main() {
         }
     }
 
-
+    // Calculate metrics with correct logic
     int current_time = 0;
     for (i = 0; i < n; i++) {
+        // Handle CPU idle time
         if (current_time < at[i]) {
             current_time = at[i];
         }
+        
         ct[i] = current_time + bt[i];
         current_time = ct[i];
 
@@ -51,10 +58,11 @@ int main() {
         totalwt += wt[i];
     }
 
-
-    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
+    printf("\n--- FCFS Scheduling Results ---\n");
+    printf("Process\tAT\tBT\tCT\tTAT\tWT\n");
     for (i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
+        // Print the original ID from pid[i]
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", pid[i], at[i], bt[i], ct[i], tat[i], wt[i]);
     }
 
     printf("\nAverage TAT: %.2f", totaltat / n);
